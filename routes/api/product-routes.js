5f218cddbeda9bed33dcf1d06c73b30929ee19aa
August Tornea <august.tornea@gmail.com>
2023-06-01 08:32:52 -0400

    product-routes getting one product by id | async/await

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,10 +18,23 @@ router.get('/', async (req, res) => {
   }
 });
 
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+// Get a specific product by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        { model: Category },
+        { model: Tag },
+      ]
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product with that ID' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
