8e5d01b68b0c550d967871e2c5d3f2515f745a40
August Tornea <august.tornea@gmail.com>
2023-06-01 08:49:30 -0400

    tag-routes delete tag | async/await

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -63,8 +63,22 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+// Delete a tag
+router.delete('/:id', async (req, res) => {
+  try {
+    const deleteCount = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (deleteCount === 0) {
+      res.status(404).json({ message: 'No tag with that ID' });
+      return;
+    }
+    res.status(200).json({ success: true });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
