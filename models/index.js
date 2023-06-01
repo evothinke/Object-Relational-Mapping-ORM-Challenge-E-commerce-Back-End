b2a8529ed135feec74969e0e8e0eaf709e14b794
August Tornea <august.tornea@gmail.com>
2023-06-01 08:17:39 -0400

    defining associations index.js

--- models/index.js
+++ models/index.js
@@ -1,17 +1,30 @@
-// import models
+
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+//associations between category and prodcut models
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'cascade',
+});
 
-// Categories have many Products
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'cascade',
+});
+//associations between product and tag where productTag is used as the join table.
+Product.belongsToMany(Tag, {
+  through: 'ProductTag',
+  onDelete: 'cascade',
+});
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: 'ProductTag',
+  onDelete: 'cascade'
+})
+//exporting models
 module.exports = {
   Product,
   Category,
